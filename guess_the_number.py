2ffbcd180997b24abe10f63262692491583e394d
PhoenixRulz00 <166043567+PhoenixRulz00@users.noreply.github.com>
2026-07-05 18:47:13 +0530

    Improve guessing feedback in number guessing game
    If you guess way off (more than 10 away), it will correctly say "Too Low!" or "Too High!".
    
    If you get warm (10 or fewer away), it will correctly say "Low!" or "High!".
    
    It catches text errors so that the game dosent crash.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,15 +1,26 @@
 import random
 
-number_to_guess = random.randint(1,100)
+number_to_guess = random.randint(1, 100)
 number_of_guesses =0
 
 while True:
     number_of_guesses+=1
-    guess = int(input("Guess the number between 1 and 100: "))
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
 
-    if guess<number_to_guess:
-        print("Too Low!")
-    elif guess>number_to_guess:
-        print("Too high!")
-    else:
-        print(f"Congratulations! You guessed the number in {number_of_guesses} guesses")
+        if guess<number_to_guess:
+            if number_to_guess-guess > 10:
+                print("Too Low!")
+            else:
+                print("Low!")
+        elif guess>number_to_guess:
+            if guess-number_to_guess > 10:
+                print("Too High!")
+            else:
+                print("High!")
+        else:
+            print(f"Congratulations! The Secret number is {number_to_guess}.")
+            print(f"You guessed the number in {number_of_guesses} guesses!")
+            break
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
